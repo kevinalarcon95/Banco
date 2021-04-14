87cd1afb0c7d6c724df916472a24cbb5b001d292
kevinalarcon95 <kfalarcon@unicauca.edu.co>
2021-04-14 11:14:43 -0500

    Implementacion de las clases

--- Negocio/clsClientes.py
+++ Negocio/clsClientes.py
@@ -1,3 +1,24 @@
 class Clientes:
 
-    def __init__(self):
+    def __init__(self,nombreCliente, usuarioCliente, idCliente):
+        self.nombreCliente = nombreCliente
+        self.usuarioCliente = usuarioCliente
+        self.idCliente = idCliente
+
+    def get_nombreCliente(self):
+        return self.nombreCliente
+
+    def set_nombreCliente(self,nombreCliente):
+        self.nombreCliente = nombreCliente
+
+    def get_usuarioCliente(self):
+        return self.usuarioCliente
+
+    def set_usuarioCliente(self, usuarioCliente):
+        self.usuarioCliente = usuarioCliente
+
+    def get_idCliente(self):
+        return self.idCliente
+
+    def set_idCliente(self, idCliente):
+        self.idCliente = idCliente
